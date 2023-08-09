80dbfb214cf93771135c22e5eb10f120a5fb0820
NB1355 <nahal5513@gmail.com>
2023-08-09 14:21:30 +0000

    OPT update database configuration helps maintainability and deployment in diffrent environment; if DATABASE_URL' is not available, fall back to using SQLite as the default database.

--- veriwiseweb/settings.py
+++ veriwiseweb/settings.py
@@ -5,6 +5,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 """
 import os
 import dj_database_url
+
 if os.path.isfile('env.py'):
     import env
 
@@ -72,14 +73,17 @@ WSGI_APPLICATION = "veriwiseweb.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # }
-
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
